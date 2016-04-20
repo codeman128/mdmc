b4ecd8148718b71a8ab7dbb668899274b4c9c33f
pkapovski <pavel.kapovski@ebs.com>
2016-04-20 09:07:09 +0300

    changes

--- src/main/java/com/pk/mdmc/core/IMessageBuffer.java
+++ src/main/java/com/pk/mdmc/core/IMessageBuffer.java
@@ -4,6 +4,6 @@ package com.pk.mdmc.core;
  * Created by PavelK on 4/13/2016.
  */
 public interface IMessageBuffer {
-    public Message next();
-    public void push(Message msg);
+    Message next();
+    void push(Message msg);
 }
